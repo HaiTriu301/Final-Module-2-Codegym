9a577d11c4556f96a36ae0f2a817d21d52560be7
HaiTriu301 <haitriuwork30@gmail.com>
2026-02-02 13:56:58 +0700

    Add POST object class

--- src/model/Post.java
+++ src/model/Post.java
@@ -1,5 +1,79 @@
 package model;
 
 public class Post {
+    private String id;
+    private String title;
+    private String content;
+    private String author;
+    private String createdDate;
 
+    public Post(){}
+
+    public Post(String id, String title, String content, String author, String createdDate) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.createdDate = createdDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", createdDate='" + createdDate + '\'' +
+                '}';
+    }
+
+    public String toFileString() {
+        return "Post{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", author='" + author + '\'' +
+                ", createdDate='" + createdDate + '\'' +
+                '}';
+    }
 }
