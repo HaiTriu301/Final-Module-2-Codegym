2c92b4ffd0feee0f8299cd8ccd4ea923121d053c
HaiTriu301 <haitriuwork30@gmail.com>
2026-02-02 14:13:01 +0700

    Add write and read with file

--- src/utils/FileUtils.java
+++ src/utils/FileUtils.java
@@ -1,5 +1,40 @@
 package utils;
 
-public class FileUtils {
+import model.Post;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtils {
+    private static final String FILE_NAME = "posts.txt";
+
+    public static void saveToFile(List<Post> posts){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_NAME))) {
+            for (Post p : posts) {
+                bw.write(p.toFileString());
+                bw.newLine();
+            }
+            System.out.println("Successful to write to file");
+        } catch (IOException e) {
+            System.out.println("Error writing to file");
+        }
+    }
+
+    public static List<Post> readFromFile() {
+        List<Post> posts = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] arr = line.split("\\|");
+                posts.add(new Post(arr[0], arr[1], arr[2], arr[3], arr[4]));
+            }
+            System.out.println("Successful to read from file");
+        } catch (IOException e) {
+            System.out.println("File is not exist");
+        }
+
+        return posts;
+    }
 }
