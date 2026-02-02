0cc8a3e2634792c782c6d53d2e3cf687e114fbc9
HaiTriu301 <haitriuwork30@gmail.com>
2026-02-02 14:08:51 +0700

    Add validation for id and file not empty

--- src/utils/ValidationUtils.java
+++ src/utils/ValidationUtils.java
@@ -1,5 +1,11 @@
 package utils;
 
 public class ValidationUtils {
+    public static boolean isValidId(String id){
+        return id.matches("P\\d{3}");
+    }
 
+    public static boolean isNotEmpty(String text){
+        return text != null && !text.isEmpty();
+    }
 }
